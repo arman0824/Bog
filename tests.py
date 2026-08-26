92ee2b1c9056b68cd01d3b3c99f2e78fdc090db2
arman0824 <armanushendrapratapsingh@gmail.com>
2026-08-26 22:38:10 +0530

    two new functions run python and write file added basic gaurdrails

--- tests.py
+++ tests.py
@@ -1,11 +1,9 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
-    working_dir = "Calculator"
-    print(get_file_content(working_dir, "lorem.txt"))
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat"))
-
+    working_directory = "calculator"
+    print(run_python_file(working_directory, "main.py", ["3 + 5"]))
 main()
