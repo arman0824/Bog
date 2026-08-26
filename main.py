8880b5f6f3e21555a85bd70863198b9dda4213e9
arman0824 <armanushendrapratapsingh@gmail.com>
2026-08-26 23:40:50 +0530

    tooling calling for the model test1

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     if len(sys.argv) < 2:
         print("I need a prompt")
@@ -23,14 +34,22 @@ def main():
         types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model="gemini-3.5-flash-lite",
-        contents=messages
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info
+        ]
     )
 
-    print(("-")*80)
-    print(response.text)
-    print(("-")*80)
+    config = types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+
+    response = client.models.generate_content(
+        model="gemini-3.5-flash-lite",
+        contents=messages,
+        config=config,
+    )
+
     if response is None or response.usage_metadata is None:
         print("Response is malformed")
         return
@@ -40,8 +59,13 @@ def main():
         print(f"Response metadata: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(response.text)
 
-print(get_files_info("calculator"))
+    
 
 
-#main()
+main()
